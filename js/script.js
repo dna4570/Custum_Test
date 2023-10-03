bdeebc7f9c425297e1e154a0f1067d3752cea461
L-sunsui <dna4570@gmail.com>
2023-10-03 12:11:22 +0900

    rollButtonCommit
    rollButtonCommit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,11 @@
-// $(function() {
-//   $("a").click(function(){
+let nav = document.querySelector("#navArea");
+let btn = document.querySelector(".toggle-btn");
+let mask = document.querySelector("#mask");
 
-//     alert($(this).attr("href"));
+btn.onclick = () => {
+  nav.classList.toggle("open");
+};
 
-//     return false;
-//   });
-// });
-function FixedAnime() {
-  var headerH = $('#header').outerHeight(true);
-  var scroll = $(window).scrollTop();
-  if(scroll >= headerH){
-    $('#header').addClass('fixed');
-  }else{
-    $('#header').removeClass('fixed');
-  }
-}
-
-//画面をスクロールしたら動かしたい場合の記述
-
-$('#g-navi a').click(function(){
-  var elmHash = $(this).attr('href');
-  var pos = Math.round($(elmHash).offset().top-120);
-
-  $('body,html').animate({scrollTop:pos},500);
-  return false;
-});
-
-$(window).scroll(function() {
-  FixedAnime();
-});
-
-$(window).on('lord', function(){
-  FixedAnime();
-});
+mask.onclick = () => {
+  nav.classList.toggle("open");
+};
